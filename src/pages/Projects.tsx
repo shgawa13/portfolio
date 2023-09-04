9a59a0ffe5ccb0e43432695e542afee2d59ca3a6
shgawa13 <37065523+shgawa13@users.noreply.github.com>
2023-09-05 00:12:57 +0300

    Project section

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,5 +1,13 @@
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div>
+      Projects
+      <div>C++</div>
+      <div>React</div>
+      <div>Vue</div>
+      <div>NodeJs</div>
+    </div>
+  );
 };
 
 export default Projects;
